8d768cccde5666737b167c2ef41769f48dfcc3aa
Shirlee Wohl <24323246+shwohl@users.noreply.github.com>
2020-03-16 16:00:05 -0400

    Modify demux script to restart from anywhere

--- scripts/batch_demux.sh
+++ scripts/batch_demux.sh
@@ -16,42 +16,55 @@ NUM_CORES=$3 # number of processors available
 #### VALUES THAT CAN BE CONFIGURED ####
 
 FASTQ="$DIR/basecalled" # assumed path to raw fastq files
-NUM_FILES=20 # number of fastq files per subdirectory
+NUM_FILES=50 # number of fastq files per subdirectory
 
 
 #### BEGIN DEMUX SCRIPT ####
 
+date
+
 #### SPLIT FILES INTO SUBDIRECTORIES ####
 
-mkdir $FASTQ/bins
-cd $FASTQ/bins/
-declare -a bins
+# if fastq files have not yet been sorted into bins
+if [ ! -d $FASTQ/bins/ ]
+	then
+		
+		# make a new subdirectory
+		mkdir $FASTQ/bins
+		cd $FASTQ/bins/
 
-COUNTER=0
-for f in $FASTQ/*.fastq; do
-	d=bin_$(printf %03d $(($COUNTER/$NUM_FILES+1))) # determine the name of the directory
-	bins+=( $d ) # add directory name to array to save for later
-	mkdir -p $d # make the new directory
-	mv $f $d # move the current fastq file to the directory
-	COUNTER=$((COUNTER + 1)) # increase the counter
-done
+		# loop through fastq files and sort them into appropriate bins
+		COUNTER=0
+		for f in $FASTQ/*.fastq; do
+			d=bin_$(printf %03d $(($COUNTER/$NUM_FILES+1))) # determine the name of the directory
+			mkdir -p $d # make the new directory
+			mv $f $d # move the current fastq file to the directory
+			COUNTER=$((COUNTER + 1)) # increase the counter
+		done
 
-echo "moved all the files, beginning demux"
+		echo "moved all the files, beginning demux"
 
+	# if the fastq files have already been sorted (this is a restart of the demux script)
+	else
+		echo "resuming demux"
+fi
 
 #### RUN PORECHOP ON EACH SUBDIRECTORY ####
 
-uniq=($(printf "%s\n" "${bins[@]}" | sort -u | tr '\n' ' ')) # get unique bins
-declare -a demux_dirs
+# bin directories are removed after their demux is completed
+# so we cant just loop through the remaining directories
 
-for d in "${uniq[@]}";
+for d in $FASTQ/bins/*/
 do
-	echo "$d demux starting"
-	porechop --format fastq.gz -b $DIR/demux/$d -i $FASTQ/bins/$d -t $NUM_CORES
-	demux_dirs+=( $DIR/demux/$d )
-	mv $FASTQ/bins/$d/* $FASTQ/ # add code to move files out of bins directory
-	rmdir $FASTQ/bins/$d
+	b=${d::-1}
+	b=${b##*/}
+	echo "$b demux starting"
+	porechop --format fastq.gz -b $DIR/demux/$b -i $FASTQ/bins/$b -t $NUM_CORES
+	mv $FASTQ/bins/$b/* $FASTQ/ # add code to move files out of bins directory
+	rmdir $FASTQ/bins/$b
 done
+
+echo "demux complete, concatenating files"
 
 #### CONCATENATE RESULTS FROM ALL BINS ####
 
@@ -59,12 +72,12 @@ while read line; do
 	b=$(echo $line | awk '{print $2}')
 	echo "BARCODE $b"
 	declare -a fq	
-	for d in "${demux_dirs[@]}"; do fq+=( $d/$b.fastq.gz ); done
+	for d in $DIR/demux/*/; do fq+=( $d$b.fastq.gz ); done
 	cat ${fq[@]} > $DIR/demux/$b.fastq.gz	
 	unset fq
 done < $SAMPLE_SHEET
 
-echo "demux portion complete, now rename files"
+echo "concatenating files complete, renaming files"
 
 # rename output files according to sample sheet
 
@@ -74,4 +87,5 @@ while read line; do
 	mv $DIR/demux/$BARCODE $DIR/demux/$SAMPLENAME
 done < $SAMPLE_SHEET
 
+date
 echo "batch demux completed successfully"
